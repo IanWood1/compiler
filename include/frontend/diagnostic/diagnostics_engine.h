40d7ee1f6c650901ca8781960b57c8b6d99055d1
Ian Wood <ianwood2024@u.northwestern.edu>
2024-06-09 17:41:58 -0700

    small work on diagnostic

--- include/frontend/diagnostic/diagnostics_engine.h
+++ include/frontend/diagnostic/diagnostics_engine.h
@@ -1,9 +1,43 @@
-/*
-The design of the DiagnosticsEngine and related classes is inspired by Clang/LLVM.
-The original source code can be found at https://github.com/llvm/llvm-project/tree/main/clang/include/clang/Basic
-*/
-
 #pragma once
+#include <map>
+#include <string>
+#include <utility>
+
 namespace diagnostic {
-class DiagnosticsEngine {};
+
+// Each diagnostic level
+enum class Level {
+#define DiagnosticLevel(level) level,
+#include "frontend/diagnostic/DiagnosticLevels.def"
+#undef DiagnosticLevel
+};
+
+// Map each level to itsself
+const static std::map<Level, Level> kDefaultLevelMapping = {
+#define DiagnosticLevel(level) {Level::level, Level::level},
+#include "frontend/diagnostic/DiagnosticLevels.def"
+#undef DiagnosticLevel
+};
+
+class DiagnosticsEngine {
+ public:
+  static DiagnosticsEngine& get() {
+    static DiagnosticsEngine diag_engine;
+    return diag_engine;
+  }
+
+  // Allow changing warning levels (Wall)
+  void setMapping(std::map<Level, Level> levelMap) {
+    mapping_ = std::move(levelMap);
+  }
+
+  void emit(Level level, const std::string& msg) {
+    Level mapped_level = mapping_[level];
+    (void)mapped_level;
+    (void)msg;
+  }
+
+ private:
+  std::map<Level, Level> mapping_ = kDefaultLevelMapping;
+};
 }  // namespace diagnostic
