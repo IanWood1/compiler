12dbfc9869e62ef59402af0c1a098de6720e9a3e
Ian Wood <ianwood2024@u.northwestern.edu>
2024-12-28 08:43:58 -0800

    Pin to LLVM 15 and update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
+
+set -euxo pipefail
+
 # Script used to build CMake Release files.
 echo "Started building Release CMAKE files..."
 
+sudo apt install llvm-15-dev
+
 # Clearing build folder if it exists
-rm -r build/ &> /dev/null
+rm -r build/ &>/dev/null
 
 cmake -B build -G Ninja
 RESULT=$?
 if [ "$RESULT" -ne 0 ]; then
-    exit $RESULT
+  exit $RESULT
 fi
 
 echo "Finished building Release CMAKE files!"
 
 if [ "$(uname -s)" == "Linux" ]; then
-    cpu_count=$(nproc)
+  cpu_count=$(nproc)
 elif [ "$(uname -s)" == "Darwin" ]; then
-    cpu_count=$(sysctl -n hw.ncpu)
+  cpu_count=$(sysctl -n hw.ncpu)
 else
-    exit 1
+  exit 1
 fi
 
 echo "Bulding Release version..."
-cmake --build build/ 
+cmake --build build/
 RESULT=$?
 echo "Finished building Release!"
 exit $RESULT
-
