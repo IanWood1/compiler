2e19511469f6bb5ab05d9bbeeaf1cdd989251cc8
Ian Wood <ianwood2024@u.northwestern.edu>
2024-12-28 08:46:17 -0800

    Fixup build/test scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,32 +2,13 @@
 
 set -euxo pipefail
 
-# Script used to build CMake Release files.
-echo "Started building Release CMAKE files..."
+echo "Started building CMAKE files..."
 
-sudo apt install llvm-15-dev
+cmake -B build \
+  -G Ninja \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-# Clearing build folder if it exists
-rm -r build/ &>/dev/null
-
-cmake -B build -G Ninja
-RESULT=$?
-if [ "$RESULT" -ne 0 ]; then
-  exit $RESULT
-fi
-
-echo "Finished building Release CMAKE files!"
-
-if [ "$(uname -s)" == "Linux" ]; then
-  cpu_count=$(nproc)
-elif [ "$(uname -s)" == "Darwin" ]; then
-  cpu_count=$(sysctl -n hw.ncpu)
-else
-  exit 1
-fi
-
-echo "Bulding Release version..."
 cmake --build build/
-RESULT=$?
-echo "Finished building Release!"
-exit $RESULT
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Script used to run tests.
+
+set -euxo pipefail
 
 echo "Building test deps"
-cmake --build build --target compiler_tests 
+cmake --build build --target compiler_tests
 echo "Starting tests..."
 cmake --build build --target test
 
-RESULT=$?
 echo "Finished running tests..."
-exit $RESULT
